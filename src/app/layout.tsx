1dccb6b729b68071301ee548d89b5bdd52c0ae24
rubek <rubek@ottr.com.np>
2023-02-13 17:08:37 +0545

    💄 UI(footer): change footer UI layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,22 +52,23 @@ export default function RootLayout({
             <button className="sc__btn">Join now</button>
           </div>
 
+          <div className="container">
+            <Image
+              src="/logo-light-background.svg"
+              alt="Strapi community logo"
+              width={876}
+              height={212}
+              className={styles.logo + " mb-20"}
+            />
+          </div>
+
           <div className="container sc-grid mb-24">
-            <div className="col-span-5">
-              <Image
-                src="/logo-light-background.svg"
-                alt="Strapi community logo"
-                width={876}
-                height={212}
-                className={styles.logo + " mb-20"}
-              />
+            <div className="col-span-6">
+              <h2 className="sc-heading--three mb-10">
+                Join our email list to stay up to date on features and releases.
+              </h2>
 
-              <p className="mb-6">
-                Subscribe to our newsletter to stay in touch with our latest
-                updates.
-              </p>
-
-              <form className="flex gap-4">
+              <form className="flex gap-4 mb-4 mr-7">
                 <input
                   type="email"
                   placeholder="What's your email?"
@@ -77,9 +78,15 @@ export default function RootLayout({
                   Subscribe
                 </button>
               </form>
+
+              <p className={styles.footerHelperText}>
+                By subscribing you agree to with our privacy policy and provide
+                consent to receive updates from our company.
+              </p>
             </div>
 
-            <div className="col-start-10 col-span-3">
+            <div className="col-start-11 col-span-2">
+              <p className={styles.footerQuickLinksTypography}>Quick Links</p>
               <ul className="flex flex-col gap-2 mb-14">
                 <li className={styles.footerLink}>
                   <Link href="/about" className={styles.footerLinkTypography}>
@@ -105,26 +112,9 @@ export default function RootLayout({
                   </Link>
                 </li>
               </ul>
-
-              <ul className="flex flex-col gap-2">
-                <li className={styles.footerLink}>
-                  <Link href="#" className={styles.footerLinkTypography}>
-                    Terms & Services
-                  </Link>
-                </li>
-                <li className={styles.footerLink}>
-                  <Link href="#" className={styles.footerLinkTypography}>
-                    Privacy Policy
-                  </Link>
-                </li>
-              </ul>
             </div>
-          </div>
 
-          <div className="container sc-grid items-center mb-10">
-            <p className="col-span-4">© 2023, Strapi. All rights reserved</p>
-
-            <div className="col-span-4 flex gap-2 justify-center">
+            <div className="col-span-4 flex gap-2">
               <button className="sc__btn--circular">
                 <BsGithub size={20} color="#666687" />
               </button>
@@ -150,8 +140,22 @@ export default function RootLayout({
                 <BsYoutube size={20} color="#666687" />
               </button>
             </div>
+          </div>
 
-            <p className="col-start-10 col-span-3">Designed by Ottr</p>
+          <div className="container sc-grid items-center mb-10">
+            <p className="col-span-4">© 2023, Strapi. All rights reserved</p>
+
+            <div className="col-span-4 flex justify-center gap-2">
+              <Link href="#" className={styles.footerFootLinks}>
+                Terms & Services
+              </Link>
+              •
+              <Link href="#" className={styles.footerFootLinks}>
+                Privacy Policy
+              </Link>
+            </div>
+
+            <p className="col-start-11 col-span-2">Designed by Ottr</p>
           </div>
         </footer>
       </body>
