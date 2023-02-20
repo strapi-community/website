3f19dee87971b56304bc620c9b5d1217262d6a3a
rubek <rubek@ottr.com.np>
2023-02-20 11:04:07 +0545

    📱 Iphone(header): setup header for mobile

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -2,30 +2,51 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { BiMenu, BiX } from "react-icons/bi";
 import { clsx } from "clsx";
 import styles from "./Header.module.scss";
 
+const LINKS = [
+  { label: "About", href: "/about" },
+  { label: "Bounty", href: "/bounties" },
+  { label: "Showcases", href: "/showcases" },
+];
+
 export const Header = () => {
   const [animateHeader, setAnimateHeader] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const listener = () => {
+    const scrollListener = () => {
       if (window.scrollY > 0) setAnimateHeader(true);
       else setAnimateHeader(false);
     };
+    window.addEventListener("scroll", scrollListener);
 
-    window.addEventListener("scroll", listener);
+    const resizeListener = () => setIsMobile(window.innerWidth < 640);
+    window.addEventListener("resize", resizeListener);
+
     return () => {
-      window.removeEventListener("scroll", listener);
+      window.removeEventListener("scroll", scrollListener);
+      window.removeEventListener("resize", resizeListener);
     };
   }, []);
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  useEffect(() => {
+    if (showMenu && isMobile) document.body.style.overflow = "hidden";
+    else document.body.style.overflow = "auto";
+  }, [showMenu, isMobile]);
+
+  const handleMenuClick = () => setShowMenu(!showMenu);
 
   return (
     <header
       className={clsx(
         styles.header,
         "transition ease-in-out duration-200",
-        animateHeader && "bg-white shadow-md"
+        (animateHeader || showMenu) && "bg-white shadow-md"
       )}
     >
       <div className={styles.nav}>
@@ -39,20 +60,35 @@ export const Header = () => {
           />
         </Link>
 
-        <nav>
+        <nav className="hidden sm:block">
           <ul className={styles.links}>
-            <li className={styles.link + " sc-link"}>
-              <Link href="/about">About</Link>
-            </li>
-            <li className={styles.link + " sc-link"}>
-              <Link href="/bounties">Bounty</Link>
-            </li>
-            <li className={styles.link + " sc-link"}>
-              <Link href="/showcases">Showcases</Link>
-            </li>
+            {LINKS.map(({ label, href }) => (
+              <li className={styles.link + " sc-link"} key={label}>
+                <Link href={href}>{label}</Link>
+              </li>
+            ))}
           </ul>
         </nav>
+
+        <button
+          className="sm:hidden h-11 w-11 flex justify-center items-center"
+          onClick={handleMenuClick}
+        >
+          {!showMenu ? <BiMenu size={24} /> : <BiX size={24} />}
+        </button>
       </div>
+
+      {showMenu && (
+        <nav className="sm:hidden w-full mt-4">
+          <ul className={styles.links}>
+            {LINKS.map(({ label, href }) => (
+              <li className={styles.link} key={label}>
+                <Link href={href}>{label}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
