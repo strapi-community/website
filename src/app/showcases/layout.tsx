cf089934bedd2d442820e3d0c9ca50e620d06a79
rubek <rubek@ottr.com.np>
2023-02-21 15:09:12 +0545

    📱 Iphone(showcases): complete responsiveness of showcase grid

--- src/app/showcases/layout.tsx
+++ src/app/showcases/layout.tsx
@@ -4,11 +4,9 @@ export default function ShowcaseLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="container sc-header-offset">
-      <div>
-        <h1 className="sc-heading--two mb-8">Showcases</h1>
-        {children}
-      </div>
+    <section className="container sc-header-offset mb-28 sm:mb-40">
+      <h1 className="sc-heading--two mb-8">Showcases</h1>
+      {children}
     </section>
   );
 }
